c227c3513cbcbecad3a9519dc5e649c2e5166dcf
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-07-23 21:27:52 +0800

    refactor: Move PDF validation checkbox to JavaScript (unobtrusive JS principles)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const $linksDiv = $('div#links');
+const $pdfCheckbox = '<label><input type="checkbox"> Allow PDF downloads</label)';
 const $odd = $('a:odd');
 const $secureLinks = $('a[href^="https://"');
 const $pdfs = $('a[href$="pdf"');
@@ -8,6 +10,12 @@ $pdfs.attr('download', 'jquery_cheatsheet.pdf');
 
 $secureLinks.addClass('secure');
 $pdfs.addClass('pdf');
+
+// const pdfCheckboxHtml = '<input type="checkbox"> Allow PDF downloads';
+// const $pdfCheckboxElement = $('<label></label)');
+// $pdfCheckboxElement.html(pdfCheckboxHtml);
+// $linksDiv.append($pdfCheckboxElement);
+$linksDiv.append($pdfCheckbox);
 
 $pdfs.on('click', (event) => {
   if ($(':checked').length === 0) {
